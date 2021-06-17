06c410b597735166f63f5ad215a2f63f1f3de6b3
MarkusSchencker <35331193+MarkusSchencker@users.noreply.github.com>
2021-06-18 00:12:54 +0200

    added further OUs

--- baselab_LabStructure.ps1
+++ baselab_LabStructure.ps1
@@ -1,6 +1,8 @@
+$ErrorActionPreference = "continue"
+
 #Setup Variables
-$DCRoot = "DC=ad,DC=lab"
-$LabDCRoot = "OU=Lab,DC=ad,DC=Lab"
+$DCRoot = "DC=schencker,DC=org"
+$LabDCRoot = "OU=Lab,DC=schencker,DC=org"
 #Create Root Lab OU
 New-ADOrganizationalUnit -Name "Lab" -Path $DCRoot -ProtectedFromAccidentalDeletion $False -Description "Lab Environment"
 #Create Other OUs
@@ -11,20 +13,22 @@ New-ADOrganizationalUnit -Name "WVD" -Path $LabDCRoot -ProtectedFromAccidentalDe
 New-ADOrganizationalUnit -Name "Computers" -Path $LabDCRoot -ProtectedFromAccidentalDeletion $False -Description "Lab Computers"
 New-ADOrganizationalUnit -Name "ANF" -Path $LabDCRoot -ProtectedFromAccidentalDeletion $False -Description "Lab ANF Objects"
 
-$WVDLabDCRoot = "OU=WVD,OU=Lab,DC=ad,DC=Lab"
+$WVDLabDCRoot = "OU=WVD,$($LabDCRoot)"
 New-ADOrganizationalUnit -Name "Computers" -Path $WVDLabDCRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Computers"
 New-ADOrganizationalUnit -Name "Users" -Path $WVDLabDCRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Users"
 New-ADOrganizationalUnit -Name "Groups" -Path $WVDLabDCRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Groups"
 New-ADOrganizationalUnit -Name "Users" -Path $WVDLabDCRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Users"
 
-$WVDComputersLabDCRoot = "OU=Computers,OU=WVD,OU=Lab,DC=ad,DC=Lab"
-New-ADOrganizationalUnit -Name "Prod" -Path "OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Desktop-1" -Path "OU=Desktop-1,OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Desktop-2" -Path "OU=Desktop-2,OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Apps-1" -Path "OU=Apps-1,OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Apps-2" -Path "OU=Apps-2,OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "VDI" -Path "OU=VDI,OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Test" -Path "OU=Test,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Desktop" -Path "OU=Desktop,OU=Test,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Apps" -Path "OU=Apps,OU=Test,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
-New-ADOrganizationalUnit -Name "Template" -Path "OU=Template,$WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+$WVDComputersLabDCRoot = "OU=Computers,$($WVDLabDCRoot)"
+New-ADOrganizationalUnit -Name "Prod" -Path "$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Desktop-1" -Path "OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Desktop-2" -Path "OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Apps-1" -Path "OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Apps-2" -Path "OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "VDI" -Path "OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Storage" -Path "OU=Prod,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Test" -Path "$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Desktop" -Path "OU=Test,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Apps" -Path "OU=Test,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Storage" -Path "OU=Test,$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
+New-ADOrganizationalUnit -Name "Template" -Path "$($WVDComputersLabDCRoot)" -ProtectedFromAccidentalDeletion $False
